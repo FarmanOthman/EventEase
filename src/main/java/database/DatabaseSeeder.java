8f9f934183c89256f249a710cb9bd80482cc9249
FarmanOthman <farmaanothmaan@gmail.com>
2025-02-22 14:29:34 +0300

    Add Comment

---
+++ src/main/java/database/DatabaseSeeder.java
@@ -0,0 +1,5 @@
+// Note: This database seeder is currently set up for testing purposes only.
+// Please do not run this seeder during development or production until testing
+// is complete.
+// Once testing is finished and the data is verified, you can use this seeder as
+// needed.
