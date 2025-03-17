955819d1ee2a05aa0e68a123ef82c6203eb074f3
AbdullahGailani <abdullahgailani004@gmail.com>
2025-03-17 21:10:46 +0300

    Implement Sale analysis

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,10 +1,14 @@
 package server;
 
 import ui.MainFrame;
-import javax.swing.SwingUtilities;
+import javax.swing.SwingUtilities;;
 
 public class Server {
     public static void main(String[] args) {
+        SalesAnalysis salesAnalysis = new SalesAnalysis();
+        
+        System.out.println(salesAnalysis.getSalesReportData());
+
         SwingUtilities.invokeLater(() -> {
             MainFrame mainFrame = new MainFrame();
             mainFrame.setVisible(true);
